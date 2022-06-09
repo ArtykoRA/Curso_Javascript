4f0bc520ee6ebfc6935622abae90e780fc078205
JuanMMalo <ac.art.yco@gmail.com>
2022-06-09 08:26:16 +0200

    cambios a carpetas y garaje

---
+++ Javascript/Pildoras/Jquery/jqueryuno.js
@@ -0,0 +1,14 @@
+
+
+
+$(document).ready(function(){
+    $("body").hide().fadeIn(5678);
+  //  $("#carrete img").hide(); //loop automatico recoge a todos los elementos
+  $("#anuncio").width(800).height(120);
+});
+
+
+
+
+
+
--- Javascript/garaje.js
+++
@@ -1,5 +0,0 @@
-document.write("hola");
-
-
-
-ctx.begingpath
